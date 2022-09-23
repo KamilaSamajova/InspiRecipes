a23f56cca2ec9da087fccaf5fa835b93cfb88790
Kamila Samajova <xsamajov@fi.muni.cz>
2022-09-23 10:38:52 +1000

    recipes edited, faq page created

--- InspiRecipes/vue/faq.js
+++ InspiRecipes/vue/faq.js
@@ -3,14 +3,54 @@ let faq = new Vue({
     data() {
         return {
             posts: [
-                { name: "", message: "" },
-                { name: "", message: "" },
-                { name: "", message: "" },
-                { name: "", message: "" },
-                { name: "", message: "" },
-                { name: "", message: "" },
-                { name: "", message: "" },
+                { topicId: 0,
+                  topic: "Topic 1", 
+                  newId: 5,
+                  newName: '',
+                  newMessage: '',                  
+                  messages: [
+                    { id: 0, name: "admin", message: "Message content message content" },
+                    { id: 1, name: "user1", message: "Message content message content" },
+                    { id: 2, name: "admin", message: "Message content message content" },
+                    { id: 3, name: "user1", message: "Message content message content" },
+                    { id: 4, name: "user2", message: "Message content message content" },
+                ]},
+                { topicId: 1,
+                  topic: "Topic 2", 
+                  newId: 7,
+                  newName: '',
+                  newMessage: '',                  
+                  messages: [
+                    { id: 0, name: "admin", message: "Message content message content" },
+                    { id: 1, name: "user1", message: "Message content message content" },
+                    { id: 2, name: "user2", message: "Message content message content" },
+                    { id: 3, name: "user3", message: "Message content message content" },
+                    { id: 4, name: "user1", message: "Message content message content" },
+                    { id: 5, name: "admin", message: "Message content message content" },
+                    { id: 6, name: "user3", message: "Message content message content" },
+                ]},
+                { topicId: 2,
+                  topic: "Topic 3", 
+                  newId: 3,
+                  newName: '',
+                  newMessage: '',
+                  messages: [
+                    { id: 0, name: "admin", message: "Message content message content" },
+                    { id: 1, name: "user3", message: "Message content message content" },
+                    { id: 2, name: "user2", message: "Message content message content" },
+                ]}
             ],
         }
     },
+    methods: {
+        addComment(topic) {
+            this.posts[topic].messages.push({ name: this.posts[topic].newName, 
+                                message: this.posts[topic].newMessage, 
+                                id: this.posts[topic].newId});
+            
+            this.posts[topic].newName = '';
+            this.posts[topic].newMessage = '';
+            this.posts[topic].newId++;
+        },
+    }
 });
