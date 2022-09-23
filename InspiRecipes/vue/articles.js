4aca425fdc148366af1265f18a1ebb870f688aa2
Kamila Samajova <xsamajov@fi.muni.cz>
2022-09-23 12:35:51 +1000

    main page articles

---
+++ InspiRecipes/vue/articles.js
@@ -0,0 +1,22 @@
+let articles = new Vue({
+    el: "#app",
+    data() {
+        return {
+            show: 0,
+            articles: [
+                { articleId: 1, name: "Article 1", image: '../Pictures/Article.jpg',
+                  article: "Article content. Article content. Article content. Article content. Article content. \
+                  Article content. Article content. Article content. Article content. Article content." },
+                { articleId: 2, name: "Article 2", image: '../Pictures/Article.jpg',
+                  article: "Article content. Article content. Article content. Article content. Article content. \
+                  Article content. Article content. Article content. Article content. Article content." },
+                { articleId: 3, name: "Article 3", image: '../Pictures/Article.jpg',
+                  article: "Article content. Article content. Article content. Article content. Article content. \
+                  Article content. Article content. Article content. Article content. Article content." },
+                { articleId: 4, name: "Article 4", image: '../Pictures/Article.jpg',
+                  article: "Article content. Article content. Article content. Article content. Article content. \
+                  Article content. Article content. Article content. Article content. Article content." },
+            ],
+        }
+    },
+});
